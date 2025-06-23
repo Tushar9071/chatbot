aa7a4a084532aee61ac0c3bcc976b065ece72551
Tushar9071 <tusharrajpara00@gmail.com>
2025-06-23 18:56:30 +0530

    23-06-2025

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import "./App.css";
 import Chatbox from "./components/whatsapp/chatbox";
 import FloatingIcon from "./components/whatsapp/floatingIcon";
 import { useWidgetConfig } from "./context/WidgetConfigContext";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   const { id, name } = useWidgetConfig();
   const [toggel, setToggle] = useState(false);
   console.log(id, name);
-  <SpeedInsights/>
 
   return (
     <>
+      <SpeedInsights />
+
       <AnimatePresence>{toggel && <Chatbox key="chatbox" />}</AnimatePresence>
       <FloatingIcon setToggle={setToggle} toggel={toggel} />
     </>
