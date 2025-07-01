bcd688fb32320406025e7899784d38fe356f87be
Tushar9071 <tusharrajpara00@gmail.com>
2025-07-01 18:37:48 +0530

    01-07-2025

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
   server: {
     port: 3000,
+    open: true,
   },
   build: {
-    cssCodeSplit: false,
-    assetsInlineLimit: 100000000,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"),
-      formats: ["iife"],
-      name: "MyWidget",
-      fileName: () => "widget.js",
-    },
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("production"),
-    "process.env": {}, // prevent other errors
+    outDir: 'dist',
   },
 });
